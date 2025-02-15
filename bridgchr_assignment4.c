5510b668a676051d40d0d460651813203dc280f6
cbridges <cbridges215@gmail.com>
2025-02-14 18:33:01 -0600

    fixed formatting

--- bridgchr_assignment4.c
+++ bridgchr_assignment4.c
@@ -11,23 +11,23 @@
 #define INPUT_LENGTH 2048
 #define MAX_ARGS 512
 
-bool bg_mode = true;  // tracks if background mode is enabled
+bool bg_mode = true;         // tracks if background mode is enabled
 bool switch_sigtstp = false; // tracks if background mode is switched to print notification
 
-/* 
+/*
  * This struct is based upon the provided parser starter code
  */
 struct command_line
 {
   char *argv[MAX_ARGS + 1]; // argument list
-  int argc; // argument count
-  char *input_file;  // input file for < redirection
-  char *output_file; // output file for > redirection
-  bool is_bg; // status of fg/bg process
+  int argc;                 // argument count
+  char *input_file;         // input file for < redirection
+  char *output_file;        // output file for > redirection
+  bool is_bg;               // status of fg/bg process
 
-  struct sigaction *sigint_enable;  // sigaction handler for ctrl-c
-  
-  struct sigaction *sigtstp_control;  // sigaction handler for ctrl-z bg mode
+  struct sigaction *sigint_enable; // sigaction handler for ctrl-c
+
+  struct sigaction *sigtstp_control; // sigaction handler for ctrl-z bg mode
 };
 
 int execute_process(struct command_line *command, pid_t *bgpids);
@@ -37,12 +37,12 @@ void child_handle_sigint();
 void inform_sigtstp();
 
 /*
- * This function based upon the provided parser starter code 
+ * This function based upon the provided parser starter code
  */
 struct command_line *parse_input()
 {
   char input[INPUT_LENGTH];
-  struct command_line *curr_command = (struct command_line *) calloc(1, sizeof(struct command_line));
+  struct command_line *curr_command = (struct command_line *)calloc(1, sizeof(struct command_line));
 
   // get input
   printf(": ");
@@ -51,22 +51,25 @@ struct command_line *parse_input()
 
   // tokenize input
   char *token = strtok(input, " \n");
-  while(token)
+  while (token)
   {
-    if(!strcmp(token, "<"))
+    if (!strcmp(token, "<"))
     {
       curr_command->input_file = strdup(strtok(NULL, " \n"));
-    } else if (!strcmp(token, ">"))
+    }
+    else if (!strcmp(token, ">"))
     {
       curr_command->output_file = strdup(strtok(NULL, " \n"));
-    } else if(!strcmp(token, "&"))
+    }
+    else if (!strcmp(token, "&"))
     {
       curr_command->is_bg = true;
-    } else
+    }
+    else
     {
       curr_command->argv[curr_command->argc++] = strdup(token);
     }
-    token=strtok(NULL, " \n");
+    token = strtok(NULL, " \n");
   }
   return curr_command;
 }
@@ -99,24 +102,24 @@ int main()
   sigaction(SIGTSTP, &sigtstp_control, NULL);
 
   struct command_line *curr_command;
-  int last_status = 0; //exit status of most recent process
+  int last_status = 0; // exit status of most recent process
 
   pid_t bgpids[25] = {0}; // array to track bg pids
 
-  // loops for cli prompt until exit 
-  while(true)
+  // loops for cli prompt until exit
+  while (true)
   {
     // checks if any bg processes have terminated
     int bg_status = checkbg(bgpids);
-    if (bg_status != -1)   // updates status with bg exit code
+    if (bg_status != -1) // updates status with bg exit code
     {
-      last_status = bg_status; 
+      last_status = bg_status;
     }
 
     // checks if bg/fg mode has been toggled
     if (switch_sigtstp)
     {
-      inform_sigtstp(); 
+      inform_sigtstp();
     }
 
     // prompts user and parses input
@@ -126,10 +129,9 @@ int main()
     curr_command->sigint_enable = &sigint_action;
     curr_command->sigtstp_control = &sigtstp_control;
 
-
     if (curr_command->argv[0] == NULL) // handles empty line
     {
-     continue; 
+      continue;
     }
     else if (!strcmp(curr_command->argv[0], "exit")) // handles exit cmd
     {
@@ -137,7 +139,7 @@ int main()
     }
     else if (!strncmp(curr_command->argv[0], "#", 1)) // handles comments
     {
-      continue; 
+      continue;
     }
     else if (!strcmp(curr_command->argv[0], "cd")) // handles cd cmd
     {
@@ -160,9 +162,7 @@ int main()
       last_status = execute_process(curr_command, bgpids);
     }
   }
-
 }
-
 
 /*
  * Function to execute commands from the shell.
@@ -176,108 +176,110 @@ int execute_process(struct command_line *command, pid_t *bgpids)
 {
   pid_t spawnpid = -5; // process id init to arbitrary invalid value
 
-  // forks process for parent shell and child command 
+  // forks process for parent shell and child command
   spawnpid = fork();
   int childStatus; // status of child process
-    
- switch (spawnpid) 
+
+  switch (spawnpid)
   {
-    case 0: // child fork
+  case 0: // child fork
+  {
+    // updates signal handler to ignore ctrl-z for both fg/bg processes
+    command->sigtstp_control->sa_handler = SIG_IGN;
+    sigaction(SIGTSTP, command->sigtstp_control, NULL);
+
+    // updates signal handler to enable ctrl-c on fg processes
+    if (!command->is_bg)
     {
-          // updates signal handler to ignore ctrl-z for both fg/bg processes
-        command->sigtstp_control->sa_handler = SIG_IGN;
-        sigaction(SIGTSTP, command->sigtstp_control, NULL);
+      command->sigint_enable->sa_handler = SIG_DFL;
+      sigaction(SIGINT, command->sigint_enable, NULL);
+    }
 
-        // updates signal handler to enable ctrl-c on fg processes
-        if (!command->is_bg)
-        {
-          command->sigint_enable->sa_handler = SIG_DFL;
-          sigaction(SIGINT, command->sigint_enable, NULL);
-        }
-
-        // redirects output fd if provided
-        if (command->output_file)
-        {
-          int outfd = open(command->output_file, O_WRONLY | O_CREAT | O_TRUNC, 0640);
-          if (outfd == -1)
-          {
-            printf("cannot open %s for output\n", command->output_file);
-            exit(1);
-          }
-          dup2(outfd, 1);
-        } else if (command->is_bg) // redirects bg process stdout to /dev/null if no user redirect
-        {
-          int outfd = open("/dev/null", O_WRONLY, 0640);
-          dup2(outfd, 1);
-        }
-
-        // redirects input fd if provided
-        if (command->input_file)
-        {
-          int infd = open(command->input_file, O_RDONLY);
-          if (infd == -1)
-          {
-            printf("cannot open %s for input\n", command->input_file);
-            exit(1);
-          }
-          dup2(infd, 0);
-        } else if (command->is_bg) // redirects bg process stdin to /dev/null is no user redirect
-        {
-          int infd = open("/dev/null", O_RDONLY);
-          dup2(infd, 0);
-        }
-  
-        // executes user command
-        execvp(command->argv[0], command->argv);
-        
-        // prints error on exec failure
-        perror("execv");
+    // redirects output fd if provided
+    if (command->output_file)
+    {
+      int outfd = open(command->output_file, O_WRONLY | O_CREAT | O_TRUNC, 0640);
+      if (outfd == -1)
+      {
+        printf("cannot open %s for output\n", command->output_file);
         exit(1);
-        break;
+      }
+      dup2(outfd, 1);
     }
-    default: // parent fork
+    else if (command->is_bg) // redirects bg process stdout to /dev/null if no user redirect
     {
-      if (!command->is_bg || !bg_mode) // if cmd is not bg or bg mode disabled
-      {
-        // parent waits for child process to complete
-        int child_pid = waitpid(spawnpid, &childStatus, 0); 
-
-       if (WIFEXITED(childStatus)) // if child exits normally
-        {
-          // returns child exit status
-          int status = WEXITSTATUS(childStatus);
-          return status; 
-        }
-       else if (WIFSIGNALED(childStatus)) // if child exits with signal
-        {
-          // returns child termination signal
-          int status = WTERMSIG(childStatus);
-          printf("terminated by signal %d\n", status);
-          return status;
-        }
-      }
-      else // if cmd is bg process
-      {
-        // prints child pid for bg process
-        printf("background pid is %d\n", spawnpid);
-        fflush(stdout);
-    
-        // adds child pid to actice process array
-        int i =0;
-        while (bgpids[i] != 0)
-        {
-          i++;
-        }
-        bgpids[i] = spawnpid;
-
-        //waitpid(spawnpid, &childStatus, WNOHANG);
-      }
+      int outfd = open("/dev/null", O_WRONLY, 0640);
+      dup2(outfd, 1);
     }
-      
+
+    // redirects input fd if provided
+    if (command->input_file)
+    {
+      int infd = open(command->input_file, O_RDONLY);
+      if (infd == -1)
+      {
+        printf("cannot open %s for input\n", command->input_file);
+        exit(1);
+      }
+      dup2(infd, 0);
+    }
+    else if (command->is_bg) // redirects bg process stdin to /dev/null is no user redirect
+    {
+      int infd = open("/dev/null", O_RDONLY);
+      dup2(infd, 0);
+    }
+
+    // executes user command
+    execvp(command->argv[0], command->argv);
+
+    // prints error on exec failure
+    perror("execv");
+    exit(1);
     break;
   }
+  default: // parent fork
+  {
+    if (!command->is_bg || !bg_mode) // if cmd is not bg or bg mode disabled
+    {
+      // parent waits for child process to complete
+      int child_pid = waitpid(spawnpid, &childStatus, 0);
 
- return 0; 
+      if (WIFEXITED(childStatus)) // if child exits normally
+      {
+        // returns child exit status
+        int status = WEXITSTATUS(childStatus);
+        return status;
+      }
+      else if (WIFSIGNALED(childStatus)) // if child exits with signal
+      {
+        // returns child termination signal
+        int status = WTERMSIG(childStatus);
+        printf("terminated by signal %d\n", status);
+        return status;
+      }
+    }
+    else // if cmd is bg process
+    {
+      // prints child pid for bg process
+      printf("background pid is %d\n", spawnpid);
+      fflush(stdout);
+
+      // adds child pid to actice process array
+      int i = 0;
+      while (bgpids[i] != 0)
+      {
+        i++;
+      }
+      bgpids[i] = spawnpid;
+
+      // waitpid(spawnpid, &childStatus, WNOHANG);
+    }
+  }
+
+  break;
+  }
+
+  return 0;
 }
 
 /*
@@ -312,16 +314,14 @@ int checkbg(pid_t *bgpids)
           printf("background pid %d is done: terminated by signal %d\n", bgpids[i], WTERMSIG(childStatus));
           bg_status = WTERMSIG(childStatus);
         }
- 
+
         // removes terminated pid from array
         bgpids[i] = 0;
       }
-
     }
   }
   return bg_status;
 }
-
 
 /*
  * Signal handler function to toggle background mode boolean.
@@ -333,7 +333,6 @@ void handle_sigtstp()
 {
   switch_sigtstp = true;
 }
-
 
 /*
  * Function to display current status of background toggle setting.
